580f8a76db76be703211d39649974d1e6fc05680
anar <anar_abbasov@hotmail.com>
2022-01-15 22:17:04 -0800

    added dot dash functions

--- newmain.c
+++ newmain.c
@@ -10,15 +10,16 @@
 
 // 'C' source line config statements
 
-// CONFIG
-#pragma config FOSC = EXTRCCLK  // Oscillator Selection bits (RC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, Resistor and Capacitor on RA7/OSC1/CLKIN)
-#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config FOSC = INTOSCCLK // Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
-#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
-#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (RB4/PGM pin has digital I/O function, HV on MCLR must be used for programming)
 #pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 #define _XTAL_FREQ 4000000
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
@@ -27,20 +28,26 @@
 
 void main(void) {
    
-     TRISB = 0x00;
-    PORTB = 0x00;
-    TRISA &= ~0x03;
-    PORTA &= ~0x03;
-    int i=100;
+    CMCONbits.CM = 0b111;
+     TRISBbits.TRISB0 = 0;
+      TRISBbits.TRISB1 = 0;
+     TRISBbits.TRISB5 = 0;
+  
     while(1)
     {
        /**/
-        PORTAbits.RA1=1;
-        __delay_ms(i);
-        PORTAbits.RA1=0;
-        
-    
-    
+        PORTBbits.RB0=1;
+     
+        __delay_ms(1000);
+         PORTBbits.RB1=1;
+        PORTBbits.RB0=0;
+            
+     __delay_ms(1000);
+        PORTBbits.RB5=1;
+         __delay_ms(1000);
+          PORTBbits.RB5=0;
+           PORTBbits.RB1=0;
+     
     }
     
     

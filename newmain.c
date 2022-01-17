ae3d555c71da908ad00b66f00a215233c314c075
AnarAbbasov <anar_abbasov@hotmail.com>
2022-01-16 19:25:26 -0800

    Update newmain.c
    Added "Q"

--- newmain.c
+++ newmain.c
@@ -57,6 +57,14 @@ void main(void) {
     while(1)
     {
        /**/
+        dash(); 
+        __delay_ms(1000)
+        dash(); 
+        __delay_ms(1000)
+        dot();  
+        __delay_ms(1000)
+        dash();
+        __delay_ms(1000)
         
         dash();               
         __delay_ms(1000)      ;
